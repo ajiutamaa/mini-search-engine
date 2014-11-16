cd927a366abe4327ede173bb97d7cba73a77e0e2
ajiutamaa <prasetya.ajie94@gmail.com>
2014-11-16 20:13:02 +0700

    tokenize

--- T3_1106015112.pl
+++ T3_1106015112.pl
@@ -9,5 +9,23 @@ sub index_documents
 # @arg: document
 sub tokenize
 {
-	
+	my ($document) = @_;
+	%words = ();
+	$onDocText = 0;
+	my @lines = split(/\n/, $document);
+	foreach $line (@lines){
+		if($line !~ /TEKS>/ && $line !~ /DOK>/ && $line !~ /JUDUL>/ && $line !~ /NO>/){		
+			$line =~ s/'//g;
+			$line =~ s/[[:punct:]]/ /g;
+			$line =~ s/ [\s]* / /g;
+			$line =~ s/\d//g;
+			@lineWords = split(/ /, $line);
+			foreach $w (@lineWords){
+				if(length($w) > 1){
+					$words{lc($w)}++;
+				}
+			}
+		}
+	}	
+	return keys %words;
 }
