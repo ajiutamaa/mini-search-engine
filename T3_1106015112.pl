e1529edf1557868a2561942e880c70b96c0e2aef
ajiutamaa <prasetya.ajie94@gmail.com>
2014-11-16 23:49:25 +0700

    masih error coy

--- T3_1106015112.pl
+++ T3_1106015112.pl
@@ -1,17 +1,35 @@
 #!/usr/local/bin/perl
 
+my %index = ();
+open(TEST, ">test_output.txt");
 # @arg: corpus
-index_documents();
+parse_documents();
 
-sub index_documents
+foreach $key (%index){
+	print TEST "$key => $index{$key}\n";
+	# @arr = @{$index{$key}};
+	# 
+	# print TEST "$key => ";
+	# for(my $i = 0; $i < scalar(@arr); $i++){
+		# if(exists($arr[$i])){
+			# print TEST "$arr[$i] | ";
+		# }
+		# else {
+			# print TEST "0 | ";
+		# }
+	# }
+	# print TEST "\n";
+}
+
+close(TEST);
+sub parse_documents
 {
 	open(KORPUS, "korpus_test.txt");
 	
 	# Documents data
 	my $documentText;
 	my $onDocText = 0;
-	
-	my %index = ();
+	my $docNumber = 0;
 	
 	my %docTokens;
 	
@@ -20,6 +38,8 @@ sub index_documents
 		#Updating index each time iterator find </DOK> tag
 		elsif($line =~ /<\/DOK>/){
 			%docTokens = tokenize($documentText);
+			update_index(\%docTokens, $docNumber);
+			$docNumber++;
 		}
 		
 		#Compile lines between <TEKS> tags
@@ -41,6 +61,8 @@ sub tokenize
 
 	$doc =~ s/'//g;
 	$doc =~ s/[[:punct:]]/ /g;
+	$doc =~ s/“/ /g;
+	$doc =~ s/”/ /g;
 	$doc =~ s/ [\s]* / /g;
 	$doc =~ s/\d//g;
 	@docWords = split(/ /, $doc);
@@ -53,3 +75,24 @@ sub tokenize
 	return %words;
 }
 
+# @arg: document tokens
+sub update_index
+{
+	my (%docTokens) = %{$_[0]};
+	my ($docNumber) = $_[1];
+	
+	$itr = 0;
+	foreach $keyDoc (keys %docTokens){
+		#print TEST "$keyDoc: $docTokens{$keyDoc}\n";
+		# for each term in document, update index
+		if(not exists($index{$keyDoc})){
+			$index{$keyDoc} = ();
+		}
+		@arr = ();
+		@arr = @{$index{$keyDoc}};
+		print TEST $keyDoc . "=>" . scalar(@arr) . "\n";
+		$arr[0] = $docTokens{$keyDoc};
+		print TEST $keyDoc . "=>" . scalar(@arr) . "\n";
+	}
+	print TEST "------------------TOT---------------------\n";	
+}
